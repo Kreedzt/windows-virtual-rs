207b1b296b8e7992c5f1ef34b2c5fa3e4fa57f11
Kreedzt <zhaozisong1@live.com>
2021-08-30 21:36:02 +0800

    :sparkles: Kreedzt - add keyboardAndMouse demo

--- bindings/build.rs
+++ bindings/build.rs
@@ -1,5 +1,12 @@
 fn main() {
     windows::build! {
         Windows::Win32::UI::WindowsAndMessaging::MessageBoxA,
+        Windows::Win32::UI::KeyboardAndMouseInput::{
+            INPUT,
+            SendInput,
+            INPUT_TYPE,
+            MOUSEINPUT,
+            MOUSE_EVENT_FLAGS
+        },
     };
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,30 @@
 use bindings::Windows::Win32::UI::WindowsAndMessaging::{MessageBoxA, MB_OK};
+use bindings::Windows::Win32::UI::KeyboardAndMouseInput::{INPUT, SendInput, MOUSEEVENTF_LEFTDOWN,
+INPUT_TYPE, INPUT_0, MOUSEINPUT, MOUSE_EVENT_FLAGS};
 
 fn main() {
-    println!("Hello, world!");
-
     unsafe {
-        MessageBoxA(None, "Hello", "World", MB_OK);
+        let mut input = INPUT {
+                r#type: INPUT_TYPE(0x0),
+                Anonymous: INPUT_0 {
+                    mi: MOUSEINPUT {
+                        dx: 0,
+                        dy: 0,
+                        mouseData: 0,
+                        dwFlags: MOUSE_EVENT_FLAGS(0x01),
+                        time: 0,
+                        dwExtraInfo: 0
+                    }
+                }
+        };
+        let ptr: *mut INPUT = &mut input;
+        SendInput(
+            1,
+            ptr,
+            std::mem::size_of::<INPUT>() as i32
+        );
     }
+    // unsafe {
+    //     MessageBoxA(None, "Hello", "World", MB_OK);
+    // }
 }
