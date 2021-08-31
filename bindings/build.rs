9a068aa1a21d3a7bada1da44c0140c547bac8684
Kreedzt <zhaozisong1@live.com>
2021-08-31 18:55:46 +0800

    :sparkles: Kreedzt - add get window rect demo

--- bindings/build.rs
+++ bindings/build.rs
@@ -1,13 +1,17 @@
 fn main() {
     windows::build! {
-        Windows::Win32::UI::WindowsAndMessaging::MessageBoxA,
+        Windows::Win32::UI::WindowsAndMessaging::{
+            MessageBoxA,
+            EnumWindows,
+            GetWindowTextW,
+            GetWindowRect
+        },
         Windows::Win32::UI::KeyboardAndMouseInput::{
             INPUT,
             SendInput,
             INPUT_TYPE,
             MOUSEINPUT,
             MOUSE_EVENT_FLAGS,
-            // MOUSEEVENTF_LEFTDOWN
         },
     };
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,46 @@
+use bindings::Windows::Win32::Foundation::{BOOL, HWND, LPARAM, PWSTR, RECT};
 use bindings::Windows::Win32::UI::KeyboardAndMouseInput::{
     SendInput, INPUT, INPUT_0, INPUT_MOUSE, INPUT_TYPE, MOUSEEVENTF_LEFTDOWN, MOUSEEVENTF_LEFTUP,
     MOUSEEVENTF_RIGHTDOWN, MOUSEEVENTF_RIGHTUP, MOUSEINPUT, MOUSE_EVENT_FLAGS,
 };
-use bindings::Windows::Win32::UI::WindowsAndMessaging::{MessageBoxA, MB_OK};
+use bindings::Windows::Win32::UI::WindowsAndMessaging::{
+    EnumWindows, MessageBoxA, MB_OK, WNDENUMPROC,
+    GetWindowTextW,
+    // GetWindow,
+    // GW_HWNDFIRST
+    GetWindowRect
+};
+
+extern "system" fn enum_window(window: HWND, l: LPARAM) -> BOOL {
+    unsafe {
+        let mut text: [u16; 512] = [0; 512];
+
+        let len = GetWindowTextW(window, PWSTR(text.as_mut_ptr()), text.len() as i32);
+        let text = String::from_utf16_lossy(&text[..len as usize]);
+
+        let mut rect = RECT {
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0
+        };
+        let res = GetWindowRect(window, (&mut rect) as (*mut RECT));
+
+        if !text.is_empty() {
+            println!("{}-{:?}", text, rect);
+        }
+
+        return true.into();
+    }
+}
 
 fn main() {
+    // 获取窗口
+    unsafe {
+        EnumWindows(Some(enum_window), LPARAM(0));
+    }
+
+    // 模拟点击
     unsafe {
         let input_1 = INPUT {
             r#type: INPUT_MOUSE,
@@ -35,7 +71,7 @@ fn main() {
         };
         let mut input_arr = [input_1, input_2];
         let ptr = &mut input_arr as *mut INPUT;
-        SendInput(2, ptr, std::mem::size_of::<INPUT>() as i32);
+        // SendInput(2, ptr, std::mem::size_of::<INPUT>() as i32);
     }
 
     // unsafe {
